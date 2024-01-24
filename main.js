72c1978c6be1fec4866f2be5c1f1b648622b5b99
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2024-01-24 16:28:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ createwindow = () => {
     newapp.loadFile('index.html');
 }
 app.whenReady().then(createwindow);
+
+/*
+const {app,BrowserWindow} = require('electron');
+createwindow = () => {
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        fullscreenable: false,
+        resizable: false,
+        webPreferences: {nodeIntegration: true},
+    });
+    newapp.loadFile('index.html');
+}
+app.whenReady().then(createwindow);
+const {app,BrowserWindow} = require('electron');
+createwindow = () => {
+    const newapp = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        fullscreenable: false,
+        resizable: false,
+        webPreferences: {nodeIntegration: true},
+    });
+    newapp.loadFile('index.html');
+}
+app.whenReady().then(createwindow);
+*/
